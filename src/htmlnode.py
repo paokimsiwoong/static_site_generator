d78559378f526dbb79e166f8399c3b5524cad686
paokimsiwoong <pao.kim.si.woong@gmail.com>
2025-01-23 14:41:37 +0900

    6: delete leafnode.py and parentnode.py - move the contents of deleted files to htmlnode.py[B

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -46,4 +46,47 @@ class HTMLNode():
         return f"{self.__class__.__name__}({self.tag}, {self.value}, {self.children}, None)"
         # @@@ {self.__class__.__name__}로 클래스 이름을 불러와서 표시하지 않으면
         # @@@ 상속받은 클래스마다 __repr__을 override해서 이름을 수정해야한다
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        # @@@@@@@@@ LeafNode는 Leaf이므로 children이 반드시 없고 value 값이 있어야 한다
+        # @@@@@@@@@ (tag는 None이어도 raw text로 표현 가능하므로 None 입력이 가능하지만 기본값 설정을 지워서 반드시 tag 지정하도록 한다)
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        # leafnode의 데이터를 실제 html 코드로 반환하는 함수
+        if not self.value:
+            raise ValueError("A leaf node must have a value")
+
+        if not self.tag:
+            # tag가 없으면 raw text로 반환
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        # self.props_to_html()은 self.props가 None이면 ""반환
+        # None이 아니면 " ~~~"로 맨 앞에 빈칸 한개가 있는 상태를 반환하므로 {self.tag}뒤에 빈칸 필요 없음 
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        # leaf 노드나 다른 parent 노드들을 담을 노드가 되어야 하므로
+        # tag와 children는 필수, value는 삭제
+        super().__init__(tag=tag, children=children, props=props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("A parent node must have a tag")
         
+        if not self.children:
+            raise ValueError("A parent node must have a children")
+        
+        def to_html_if_node(x):
+            if not isinstance(x, HTMLNode):
+                raise TypeError("A child must be an instance of HTMLNode")
+            # self.children 안에는 HTMLNode를 상속한 노드들만 속해 있는지 확인하기
+            
+            return x.to_html()
+        
+        return f"<{self.tag}{self.props_to_html()}>{"".join(map(to_html_if_node, self.children))}</{self.tag}>"
+        # children안의 모든 종속 노드들의 태그들을 합쳐서 이 ParentNode 태그 안에 넣기
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -29,6 +29,74 @@ class TestHTMLNode(unittest.TestCase):
         # ===>
         # with self.assertRaises(exception이름):
             # 지정한 exception이 나올지 확인할 코드
+
+
+class TestLeafNode(unittest.TestCase):
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    # Both test functions and file names must start with test_ 
+    # to be discoverable by unittest
+    # ==> 파일이름 : test_****.py, 함수이름 : def test_****(self):
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    def test_repr(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertTrue(node.__repr__() == 'LeafNode(a, Click me!, None,  href="https://www.google.com")')
+        # f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props_to_html()})"
+
+    def test_to_html(self):
+        node = LeafNode(tag = "p", value="test")
+        node2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        node3 = LeafNode(tag=None, value="raw")
+
+        self.assertTrue(
+            node.to_html() == "<p>test</p>" and
+            node2.to_html() == '<a href="https://www.google.com">Click me!</a>' and
+            node3.to_html() == "raw"
+            )
+        
+        node4 = LeafNode(tag = "p", value="")
+        with self.assertRaises(ValueError):
+            node4.to_html()
+        # value 값이 비정상일 때 valueerror 잘 나오는지 확인
+
+
+class TestParentNode(unittest.TestCase):
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    # Both test functions and file names must start with test_ 
+    # to be discoverable by unittest
+    # ==> 파일이름 : test_****.py, 함수이름 : def test_****(self):
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    def test_repr(self):
+        # 복잡하게 nested 되도 __repr__ 잘 작동하는지 확인
+        node = ParentNode("a", [LeafNode("p", "test"), ParentNode("b", [LeafNode("p", "test"), LeafNode("p", "test")])], {"href": "https://www.google.com"})
+        # print(node)
+        self.assertTrue(node.__repr__() == 'ParentNode(a, None, [LeafNode(p, test, None, None), ParentNode(b, None, [LeafNode(p, test, None, None), LeafNode(p, test, None, None)], None)],  href="https://www.google.com")')
+        # f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props_to_html()})"
+
+    def test_to_html(self):
+        node = ParentNode("a", [LeafNode("p", "test"), ParentNode("b", [LeafNode("p", "test"), LeafNode("p", "test")])], {"href": "https://www.google.com"})
+
+        # print(node.to_html())
+
+        self.assertTrue(
+            node.to_html() == '<a href="https://www.google.com"><p>test</p><b><p>test</p><p>test</p></b></a>'
+            )
+        
+        node2 = ParentNode("a", [], {"href": "https://www.google.com"})
+        with self.assertRaises(ValueError):
+            try:
+                node2.to_html()
+            except Exception as e:
+                print(e)
+                raise e
+
+            # node2.to_html()
+        # children 값이 비정상일 때 valueerror 잘 나오는지 확인
+
+        node3 = ParentNode("a", [LeafNode("p", "test"), 3], {"href": "https://www.google.com"})
+        with self.assertRaises(TypeError):
+            # children안에 HTMLNode 계열이 아닌 것이 들어 있으면 TypeError가 나야한다
+            node3.to_html()
+        # children 값이 비정상일 때 valueerror 잘 나오는지 확인
 
 
 if __name__ == "__main__":
