85b2ebd12dbcb9756bf8dee2c0e95bfcc8bdaf96
paokimsiwoong <pao.kim.si.woong@gmail.com>
2025-01-24 16:09:55 +0900

    11: update markdown_utils.py - add block_to_block_type function

--- src/markdown_utils.py
+++ src/markdown_utils.py
@@ -1,3 +1,5 @@
+import re
+
 def markdown_to_blocks(markdown):
     blocks = []
     # 함수 반환값(block str들을 담은 list)을 담을 리스트
@@ -29,3 +31,53 @@ def markdown_to_blocks(markdown):
         blocks.append("\n".join(block).strip())
 
     return blocks
+
+def block_to_block_type(block):
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    # 해답처럼 매번 각 타입을 "" str로 입력하지 말고 변수이름으로 지정가능하게 미리 만들어두기
+    type_heading = "heading"
+    type_code = "code"
+    type_quote = "quote"
+    type_unordered_list = "unordered_list"
+    type_ordered_list = "ordered_list"
+    type_paragraph = "paragraph"
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+    type_checker = {
+        r"^#+\s":type_heading,
+        r"^```(.|\n)+?```":type_code,
+        r"^>\s":type_quote,
+        r"^\*\s":type_unordered_list,
+        r"^-\s":type_unordered_list,
+        r"^[0-9]+\.\s":type_ordered_list,
+        }
+    
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    # block이 여러줄인 경우 그 모든 줄들이 동일한 type인 것을 확인해야한다
+    # ==> 아니면 그 block의 type은 paragraph
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    lines = block.split("\n")
+    
+    for k, v in type_checker.items():
+        if isinstance(re.match(k, block),re.Match):
+            if v == "heading" or v == "code":
+                return v
+            
+            for line in lines:
+                if not isinstance(re.match(k, line),re.Match):
+                    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                    # block이 여러줄인 경우 그 모든 줄들이 동일한 type인 것을 확인해야한다
+                    # ==> 아니면 그 block의 type은 paragraph
+                    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                    return type_paragraph
+            return v
+        
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    # 해답은
+    # if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        # return block_type_heading
+    # 과 같이 .startswith 함수사용
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+        
+    return type_paragraph
--- src/test_markdown_utils.py
+++ src/test_markdown_utils.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_utils import markdown_to_blocks
+from markdown_utils import markdown_to_blocks, block_to_block_type
 
 
 class TestMarkdownUtils(unittest.TestCase):
@@ -55,6 +55,34 @@ class TestMarkdownUtils(unittest.TestCase):
             # print(blocks)
 
             self.assertSequenceEqual(blocks, case[1])
+
+    
+    def test_block_to_block_type(self):
+        type_heading = "heading"
+        type_code = "code"
+        type_quote = "quote"
+        type_unordered_list = "unordered_list"
+        type_ordered_list = "ordered_list"
+        type_paragraph = "paragraph"
+
+        cases = [
+            ("# This is a heading", type_heading),
+            ("### This one is also a heading", type_heading),
+            ("```\nthis = 'code_block'\na = 3\nb = 10\n```", type_code),
+            ("> This is a quote line\n> another quote line", type_quote),
+            ("* unordered\n* list\n* testing", type_unordered_list),
+            ("- unordered\n- list\n- testing", type_unordered_list),
+            ("1. ordered\n2. list\n3. testing", type_ordered_list),
+            ("this is just a paragraph", type_paragraph),
+            ("> This is not a quote line\nanother not quote line", type_paragraph),
+            ("* unordered\nlist\n* testing", type_paragraph),
+            ("1. not ordered\nlist\ntesting", type_paragraph),
+        ]
+
+        for case in cases:
+            block_type = block_to_block_type(case[0])
+            
+            self.assertEqual(block_type, case[1])
 
 
 if __name__ == "__main__":
